dd682daf0c80b854058490273f964949838606a6
Lizzie <lchan217@gmail.com>
2020-01-06 11:37:09 -0700

    moviechar fetches each person

--- src/components/MovieChars.js
+++ src/components/MovieChars.js
@@ -4,16 +4,37 @@ class MovieChars extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      characters: []
+      characters: [],
+      isLoading: true
     };
   }
+
+  componentDidMount() {
+    {
+      this.props.movie.characters.map(char => {
+        fetch(char)
+          .then(response => response.json())
+          .then(response => console.log(response));
+      });
+    }
+    // fetch("https://swapi.co/api/films")
+    //   .then(response => response.json())
+    //   .then(response => console.log(response));
+    // .then(response => {
+    //   this.setState({
+    //     movies: response.results,
+    //     isLoading: false
+    //   });
+    // });
+  }
+
   render() {
     return (
       <div>
         <h1>{this.props.movie.title}</h1>
-        {this.props.movie.characters.map(char => {
+        {/* {this.props.movie.characters.map(char => {
           return <li>{char}</li>;
-        })}
+        })} */}
       </div>
     );
   }
