0bcc0c19b007b32544d37768ff2b4eff3485ef64
Lizzie <lchan217@gmail.com>
2020-01-06 11:01:37 -0700

    add whether or not to show char component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,14 +1,30 @@
 import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
+import MovieChars from "./MovieChars";
 
 class MovieList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showComponent: false
+    };
+    this._onCardClick = this._onCardClick.bind(this);
+  }
+
+  _onCardClick() {
+    this.setState({
+      showComponent: true
+    });
+  }
+
   render() {
     return (
       <div className='movie-card'>
-        <Card>
+        <Card onClick={this._onCardClick}>
           <Card.Header>{this.props.title}</Card.Header>
           <Card.Meta>{this.props.release_date}</Card.Meta>
         </Card>
+        {this.state.showComponent ? <MovieChars movie={this.props} /> : null}
       </div>
     );
   }
