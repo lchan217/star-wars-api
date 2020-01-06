5fa9b3f6510c153a5617b2c5e44da7ce4ac0cbdb
Lizzie <lchan217@gmail.com>
2020-01-06 13:44:30 -0700

    add promise all and setstate functionality

--- src/components/MovieChars.js
+++ src/components/MovieChars.js
@@ -4,37 +4,31 @@ class MovieChars extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      characters: [],
-      isLoading: true
+      characters: []
     };
   }
 
-  componentDidMount() {
-    {
-      this.props.movie.characters.map(char => {
-        fetch(char)
-          .then(response => response.json())
-          .then(response => console.log(response));
-      });
-    }
-    // fetch("https://swapi.co/api/films")
-    //   .then(response => response.json())
-    //   .then(response => console.log(response));
-    // .then(response => {
-    //   this.setState({
-    //     movies: response.results,
-    //     isLoading: false
-    //   });
-    // });
-  }
+  fetchChars = () => {
+    let urls = [];
+    this.props.movie.characters.map(char => {
+      urls.push(char);
+    });
+    const allRequests = urls.map(url =>
+      fetch(url).then(response => response.json())
+    );
+
+    Promise.all(allRequests).then(arrayOfResponses =>
+      this.setState({ characters: arrayOfResponses })
+    );
+  };
+
+  // this.state.characters.name
 
   render() {
     return (
       <div>
         <h1>{this.props.movie.title}</h1>
-        {/* {this.props.movie.characters.map(char => {
-          return <li>{char}</li>;
-        })} */}
+        {this.fetchChars()}
       </div>
     );
   }
