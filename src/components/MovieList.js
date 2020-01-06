68665fd767dca604f97219e82614ead2f15146a6
Lizzie <lchan217@gmail.com>
2020-01-06 10:21:57 -0700

    change movielist to component

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
 
-const MovieList = ({ title, release_date }) => {
-  return (
-    <div className='movie-card'>
-      <Card>
-        <Card.Header>{title}</Card.Header>
-        <Card.Meta>{release_date}</Card.Meta>
-      </Card>
-    </div>
-  );
-};
+class MovieList extends Component {
+  render() {
+    return (
+      <div className='movie-card'>
+        <Card>
+          <Card.Header>{this.props.title}</Card.Header>
+          <Card.Meta>{this.props.release_date}</Card.Meta>
+        </Card>
+      </div>
+    );
+  }
+}
 
 export default MovieList;
